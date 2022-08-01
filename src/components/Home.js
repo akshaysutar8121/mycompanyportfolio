32c7acbba1d22978eae7b2185ba6729ba82d2c61
akshaysutar8121 <104812414+akshaysutar8121@users.noreply.github.com>
2022-08-01 09:30:49 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,22 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import { motion, useMotionValue } from 'framer-motion';
-
+import './Home.css';
 
 const Home = () => {
   return (
     <>
-      <div className="flex mt-10">
-        <div className="inner_text w-1/2 p-10 text-sm mt-10">
-          <p className=" pr-20 pt-20 pb-20 p-3">
+      <div className="flex flex-col sm:flex-row mt-10 ">
+        <div className="inner_text  p-10 text-md sm:text-sm mt-20">
+          <p className="mt-3 pr-2 pt-2 pb-2 p-3 ">
             <span className="text-4xl font-bold text-indigo-500">
               Hii my name is Akshay sutar
             </span>
-            . I am a full stack web Devoloper this is my my personal portfolio
+            . I am a full stack web Devoloper this is my personal portfolio
             website
           </p>
         </div>
-        <div className="image w-1/2 mt-20">
+        <div className="image mt-20">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/college-project-b119a.appspot.com/o/undraw_Code_review_re_woeb.png?alt=media&token=cbec32ee-a205-4e21-8ab0-33cb9ca5530f"
             alt=""
@@ -27,8 +27,14 @@ const Home = () => {
         </div>
       </div>
       <div className="social  w-1/2 mt-10 justify-center text-center ml-20 mb-10">
-        <ul className="justify-between mb-2 text-indigo-400 flex text-center justify-center">
+        <ul className="justify-between mb-2 text-indigo-400 flex text-center justify-center sm:mt-20">
           <motion.li
+            whileTap={{
+              scale: [1.5, 3, 3, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+            }}
+            
             whileHover={{
               scale: [1.5, 3, 3, 1, 1],
               rotate: [0, 0, 270, 270, 0],
@@ -43,6 +49,12 @@ const Home = () => {
             </a>
           </motion.li>
           <motion.li
+            whileTap={{
+              scale: [1.5, 3, 3, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+            }}
+            
             whileHover={{
               scale: [1.5, 3, 3, 1, 1],
               rotate: [0, 0, 270, 270, 0],
@@ -57,6 +69,12 @@ const Home = () => {
             </a>
           </motion.li>
           <motion.li
+            whileTap={{
+              scale: [1.5, 3, 3, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+            }}
+            
             whileHover={{
               scale: [1.5, 3, 3, 1, 1],
               rotate: [0, 0, 270, 270, 0],
@@ -71,6 +89,12 @@ const Home = () => {
             </a>
           </motion.li>
           <motion.li
+            whileTap={{
+              scale: [1.5, 3, 3, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+            }}
+            
             whileHover={{
               scale: [1.5, 3, 3, 1, 1],
               rotate: [0, 0, 270, 270, 0],
